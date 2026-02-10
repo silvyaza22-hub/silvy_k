6139bb863294efaf3ef070554efbc92e6a7458c6
imahesz03 <mahesa16032006@gmail.com>
2026-02-10 15:54:08 +0700

    latihan2

--- application/controllers/hello.php
+++ application/controllers/hello.php
@@ -2,4 +2,14 @@
 
 class hello extends CI_Controller {
 
+    public function index()
+{
+    $data['nama'] = "Codeigneter 3 + php 5.6";
+    $this->load->view('hello_view' ,$data);
+}
+    public function namasaya()
+{
+    $data['nama'] = "mahesa";
+    $this->load->view('hello_view' ,$data);
+}
 }
