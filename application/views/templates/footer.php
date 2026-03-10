1d8a8ebb6d512c18e403f0f8cc0dc7a665badd73
imahesz03 <mahesa16032006@gmail.com>
2026-03-10 15:57:35 +0700

    10Mar2026

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,7 +1,8 @@
-<div>
+</div>
 </div>
 
-<sctipt src="<?= base_url('assets/vendor/jquery/jquery.min.js'); ?>"></script>
+
+<script src="<?= base_url('assets/vendor/jquery/jquery.min.js'); ?>"></script>
 <script src="<?= base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js');?>"></script>
 <script src="<?= base_url('assets/js/sb-admin-2.min.js');?>"></script>
 
